bfd147cc1843ee5058d48f8bba57f6e957b347ec
Nam Huynh <Nam.huynh.h@gmail.com>
2019-02-26 14:20:18 -0700

    finished version with scoreboard fix, game still acting funny

--- SnakeGameFinal/ScoreBoard.aspx.cs
+++ SnakeGameFinal/ScoreBoard.aspx.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Data.Common;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -14,47 +13,57 @@ namespace SnakeGameFinal
 {
     public partial class ScoreBoard : System.Web.UI.Page
     {
-       
-        MySqlCommand cmd = new MySqlCommand();
-        MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["myDB"].ConnectionString);
-        MySqlDataAdapter ada = new MySqlDataAdapter();
-        DataSet da = new DataSet();
 
+
+        StringBuilder table = new StringBuilder();
         protected void Page_Load(object sender, EventArgs e)
         {
-            con.Open();
-            cmd.CommandText = "select top 10 * from score";
-            cmd.Connection = con;
-            DataTable dt = new DataTable();
-            MySqlDataReader rd = cmd.ExecuteReader();
-            StringBuilder table = new StringBuilder();
-            table.Append("<center>");
-            table.Append("<h1>Score Board Leaders</h1>");
-            table.Append("<hr/>");
-            table.Append("<table border=1");
-            table.Append("<tr>");
-            foreach (DataColumn dc in dt.Columns)
+            if (!Page.IsPostBack)
             {
-                table.Append("<th>");
-                table.Append(dc.ColumnName.ToUpper());
-                table.Append("</th>");
-            }
-            table.Append("</tr>");
-
-            foreach(DataRow dr in dt.Rows)
-            {
+                MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["myDB"].ConnectionString);
+                string scoreQuery = "Select * From scores Order By score DESC Limit 10";
+                int rank = 1;
+                MySqlCommand cmd = new MySqlCommand(scoreQuery, con);
+                con.Open();
+                MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+                DataTable DT = new DataTable();
+                sda.Fill(DT);
+                table.Append("<h1>Top 10 Highest Scores:");
+                table.Append("<center>");
+                table.Append("<table border='1'>");
                 table.Append("<tr>");
-                foreach(DataColumn dc in dt.Columns)
+                table.Append("<th>");
+                table.Append("Ranking");
+                table.Append("</th>");
+                foreach (DataColumn dc in DT.Columns)
                 {
+
                     table.Append("<th>");
-                    table.Append(dr[dc.ColumnName].ToString());
+                    table.Append(dc.ColumnName.ToUpper());
                     table.Append("</th>");
                 }
+                table.Append("</tr>");
+                foreach (DataRow dr in DT.Rows)
+                {
+                    table.Append("<th>");
+                    table.Append(rank);
+                    table.Append("</th");
+                    table.Append("<tr>");
+                    foreach (DataColumn dc in DT.Columns)
+                    {
+
+                        table.Append("<th>");
+                        table.Append(dr[dc.ColumnName].ToString());
+                        table.Append("</th>");
+                    }
+                    table.Append("</tr>");
+                    rank++;
+                }
+                table.Append("</table>");
+                table.Append("</center>");
+                Panel1.Controls.Add(new Literal { Text = table.ToString() });
+                con.Close();
             }
-            table.Append("</tr>");
-            table.Append("</center>");
-            Panel1.Controls.Add(new Label {Text = table.ToString()});
-            con.Close();
         }
     }
 }
